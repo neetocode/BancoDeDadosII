bbc3ec12273c3c5effcc6ae39562f220595c3792
MiguelNeto <MiguelNeto>
2016-10-31 21:06:07 -0300

    Exemplo group by com having

---
+++ Exercicios/Em sala/ExemploGroupByComHaving.sql
@@ -0,0 +1,16 @@
+/*
+Selecionar o código, o total de atletas, o maior salário e a média salarial
+daqueles times que possuam salários maiores do que R$ 30.000
+*/
+
+SELECT
+jogadores.cod_time as 'Código do time',
+(select clube.nom_time from times clube where clube.cod_time = jogadores.cod_time) as 'Nome do time',
+count(*) as 'Quantidade de jogadores',
+max(jogadores.salario) as 'Maior salário',
+avg(jogadores.salario) as 'Média salarial'
+from
+times
+join jogadores on jogadores.cod_time = times.cod_time
+group by jogadores.cod_time
+having sum(jogadores.salario) > 30000
